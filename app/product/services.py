39189449d6bf94002e7e5dc104db1f08aac1fc5e
saad zaheer <salaam7860@gmail.com>
2026-02-01 15:05:58 +0500

    adding products

--- app/product/services.py
+++ app/product/services.py
@@ -1,10 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import HTTPException, status
+from fastapi import HTTPException, UploadFile, status
 from sqlalchemy import select
 
 
 from app.product.models import Product, Category
-from app.product.schemas import CategoryCreate, CategoryOut
+from app.product.schemas import CategoryCreate, CategoryOut, ProductCreate, ProductOut
+from app.product.utils import save_upload_file
 
 
 ##############################################
@@ -44,4 +45,27 @@ async def delete_cat(session: AsyncSession, id: int)->bool:
     return True
     
 
+##############################################
+################ PRODUCT #####################
+##############################################
+
+# PRODUCT CREATE
+async def create_product(session: AsyncSession, product_data: ProductCreate, image_file: UploadFile | None = None)->ProductOut:
+    # CHECK NEGATIVE STOCK QUANTITY
+    if product_data.stock_quantity < 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Stock quantity can not be negative")
+    image_path = await save_upload_file(image_file, "images")
+
+    categories = [] 
+    if product_data.category_ids:
+        category_stmt = select(Category).where(Category.id.in_(product_data.category_ids))
+        category_result = await session.execute(category_stmt)
+        categories = category_result.scalars().all()
+    
+    product_dict = product_data.model_dump(exclude={"category_ids"})
+
+# PRODUCT GET
+# PROUCT UPDATE
+# PRODUCT PATCH
+# PRODUCT DELETE
     
---
+++ app/product/utils.py
@@ -0,0 +1,69 @@
+from fastapi import UploadFile, HTTPException, status
+from slugify import slugify
+from pathlib import Path
+from uuid import uuid4
+
+UPLOAD_DIR = Path("uploads")
+ALLOWED_EXTENSIONS = {".jpg", ".jpeg", ".png", ".pdf"}
+ALLOWED_TYPES = {"image/jpeg", "image/png", "application/pdf"}
+
+# check if the directory exist or not
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+async def save_upload_file(upload_file: UploadFile, sub_dir: str)->str: 
+
+    ALLOWED_EXTENSION = [".jpg", ".jpeg", ".png", ".pdf"]
+
+    # create file extension
+    ext = Path(upload_file.filename).suffix.lower()
+
+    if ext not in ALLOWED_EXTENSION:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid file type.")
+    
+    # Fake extensionvirus.exe.png 😬
+    if upload_file.content_type not in ALLOWED_TYPES:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid content type.")
+    # create a filename
+    filename = f"{uuid4().hex}{ext}"
+    # create directory path 
+    dir_path = UPLOAD_DIR / sub_dir
+    # check if the directory exist or not
+    dir_path.mkdir(parents=True, exist_ok=True) 
+    # create file path
+    file_path = dir_path / filename
+
+    """
+    User file upload karta hai
+    FastAPI usay UploadFile object bana deta hai
+    Tum read() se uska binary data nikalte ho
+    Disk par nayi file banate ho
+    Us data ko us file mein write kar dete ho
+
+    Bonus tip (large files ke liye)
+
+    Agar file bohat bari ho, read() poori file memory mein la deta hai (danger 😬).
+    Better approach hoti hai:
+    
+    with file_path.open("wb") as f:
+    while chunk := await upload_file.read(1024):  <------------------  tip
+        f.write(chunk)
+
+    """
+
+   
+
+    # “bhai jo file user ne bheji hai uska poora data memory mein la do”
+    try: 
+        content = await upload_file.read()
+        with file_path.open("wb") as f: 
+            f.write(content)
+    except OSError as e:
+        print(f"File system error: {e}")
+    except Exception as e: 
+        print(f"Something went wrong: {e}")
+
+    return str(file_path)
+
+
+def generate_slug(text: str)->str:
+    return slugify(text)
